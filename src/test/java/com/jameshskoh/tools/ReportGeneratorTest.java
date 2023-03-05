0c3891a1653d8a0a86e36d0abb7d04698cb4770f
Koh Hui Soon <huisoonkoh@gmail.com>
2023-03-05 16:10:46 +0800

    Save changes

---
+++ src/test/java/com/jameshskoh/tools/ReportGeneratorTest.java
@@ -0,0 +1,7 @@
+package com.jameshskoh.tools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportGeneratorTest {
+
+}
---
+++ src/test/java/com/jameshskoh/tools/TextStatisticsGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.jameshskoh.tools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextStatisticsGeneratorTest {
+
+    @Test
+    void generateStatistics_shouldReturnNullWhenTextIsNull() {
+        
+    }
+}
